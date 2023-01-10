385f46fb4125354f19afb2c3568abc69afd86ac2
huyhoan1109 <huyhoan0901@gmail.com>
2023-01-10 10:12:35 +0700

    update framework

--- storage/framework/views/999c4f3eb140bc1112aaa3cc3f50502bc8506861.php
+++
@@ -1,6 +0,0 @@
-<div class="header-action-icon-2">
-    <a href="<?php echo e(route('wishlist')); ?>">
-        <img src="<?php echo e(asset('assets/imgs/theme/icons/icon-heart.svg')); ?>">
-        <span class="pro-count blue"><?php echo e(@count($items)); ?></span> 
-    </a>
-</div><?php /**PATH /home/kaneki/20221/Lập trình Web/FashionApp/resources/views/livewire/wishlist-icon-component.blade.php ENDPATH**/ ?>
